b2c4856e46847b6ce39d85db31e1e87709d59a27
ruchikaroy <contactrroy.uk@gmail.com>
2024-10-24 11:55:07 +0100

    made changes to navbar

--- src/Components/StickyNavBar.tsx
+++ src/Components/StickyNavBar.tsx
@@ -66,7 +66,7 @@ const StickyNavBar = () => {
   return (
     <>
       <div className="-m-5 max-h-[768px] w-[calc(100% + 48px)] overflow-scroll">
-        <Navbar className="sticky top-0 z-10 max-w-full rounded-none px-4 py-3 lg:px-8 lg:py-4">
+        <Navbar className="top-0 z-10 max-w-full rounded-none px-4 py-3 lg:px-8 lg:py-4">
           <div
             style={{ marginBottom: "-10px" }}
             className="flex items-center justify-between text-blue-gray-900"
